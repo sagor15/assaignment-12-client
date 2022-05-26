1b373db92e06a91c827eec07326794841ab6a40a
sagor15 <sagorahammed1562@gmail.com>
2022-05-26 21:08:04 +0600

    update review section done

--- src/Components/Pages/Review.js
+++ src/Components/Pages/Review.js
@@ -24,7 +24,7 @@ const Review = () => {
             _id: 3,
             name: "walson qury",
             loacation: "california",
-            review: "It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content",
+            review: "It is a longs established facts that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content",
             img: people3
         }
     ]
