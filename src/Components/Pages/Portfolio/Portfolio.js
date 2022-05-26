23795542ad45848750a37044cca205b4bb2bf8fc
sagor15 <sagorahammed1562@gmail.com>
2022-05-26 21:07:00 +0600

    update portfolio page

--- src/Components/Pages/Portfolio/Portfolio.js
+++ src/Components/Pages/Portfolio/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = () => {
             <p class="py-6">I’m a Junior Web Developer. I can see myself in a top-ranking
              position in the web developing market. True that this market is growing higher day by day . But my skills are also growing as I am a very keen learning and technology lover person. That’s why I find this sector very 
             interesting and always work with my full enthusiasm.</p>
-            <button class="btn btn-primary">Get Started</button>
+            <button class="btn btn-primary">started</button>
           </div>
         </div>
       </div>
